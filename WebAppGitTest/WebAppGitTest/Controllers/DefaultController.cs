be0785f91fd9c98c7e635bd7cb1d6138b270a4d1
SomaDeb <SomaDeb@DESKTOP-TJGPONU>
2018-12-01 17:42:55 +0530

    initial Commit

--- WebAppGitTest/WebAppGitTest/Controllers/DefaultController.cs
+++ WebAppGitTest/WebAppGitTest/Controllers/DefaultController.cs
@@ -11,7 +11,7 @@ namespace WebAppGitTest.Controllers
         // GET: Default
         public ActionResult Index()
         {
-            Session["NAME"] = "HELLO WOLD";
+            Session["NAME"] = "HELLO WORLD";
             return View();
         }
     }
